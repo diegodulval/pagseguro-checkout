67f3591cd6e5d954b31dd09c8b9516b1948809bb
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-05-10 18:17:47 -0300

    Create initial project config

---
+++ src/main/java/com/npdi/demo/pagseguro/PagseguroApplication.java
@@ -0,0 +1,18 @@
+package com.npdi.demo.pagseguro;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagseguroApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagseguroApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/test/java/com/npdi/demo/pagseguro/PagseguroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.npdi.demo.pagseguro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PagseguroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
