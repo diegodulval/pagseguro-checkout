254e1c6ea84b6a11e68df9be33a4406190620592
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-05-18 13:22:16 -0300

    Added transaction query in repository

--- src/main/java/com/npdi/demo/pagseguro/repositories/AgreementRepository.java
+++ src/main/java/com/npdi/demo/pagseguro/repositories/AgreementRepository.java
@@ -3,6 +3,7 @@ package com.npdi.demo.pagseguro.repositories;
 import com.npdi.demo.pagseguro.domains.Agreement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AgreementRepository extends JpaRepository< Agreement, Long> {
 
+    @Transactional(readOnly = true)
     Agreement findByCodeReference(String codeReference);
 
 }
