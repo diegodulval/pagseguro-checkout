78416e7528b54f6b0814b5917b8cfb5a8c07d23c
Diego Dulval de Freitas <diegodifreitas@gmail.com>
2018-05-18 08:20:02 -0300

    Created agreement Repository

---
+++ src/main/java/com/npdi/demo/pagseguro/domains/Agreement.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.npdi.demo.pagseguro.domains;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import lombok.Data;
+
+@Data
+@Entity
+public class Agreement implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String codePreApproval;
+    private String codeReference;
+    private String redirectURL;
+
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date startDate;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date finalDate;
+
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "plan_id")
+    @JsonIgnore
+    private Plan plan;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm", timezone = "GMT-3")
+    private Date createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm", timezone = "GMT-3")
+    private Date updatedAt;
+
+}
